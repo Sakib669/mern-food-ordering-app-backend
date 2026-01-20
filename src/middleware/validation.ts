83ebe9145ab5e0627717e7bc79b854df9b9bad7d
Shafiqul Islam Sakib <stewieboyyy@gmail.com>
2026-01-20 23:03:10 +0600

    fix: added next() in validation middleware and jwtParse

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -10,6 +10,7 @@ const handleValidationErrors = async (
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
+  next();
 };
 
 export const validateMyUserRequest = [
