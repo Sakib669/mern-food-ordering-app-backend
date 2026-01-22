c04a8eafdaabc3001efc221e2a440bbb78bd5230
Shafiqul Islam Sakib <stewieboyyy@gmail.com>
2026-01-23 00:44:01 +0600

    adding user profile features

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,19 @@ import express, { type Request, type Response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute.js";
+import { v2 as cloudinary } from "cloudinary";
+import MyRestaurantRoute from "./routes/MyRestaurantRoute.js"
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("connected to mongodb"))
   .catch((err) => console.log("Database connection error: মুরগি চোর", err));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME as string,
+  api_key: process.env.CLOUDINARY_API_KEY as string,
+  api_secret: process.env.CLOUDINARY_API_SECRET as string,
+});
 
 const app = express();
 app.use(express.json());
@@ -19,6 +27,7 @@ app.get("/health", async (req: Request, res: Response) => {
 
 // /api/my/user
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", MyRestaurantRoute)
 
 app.get("/test", async (req: Request, res: Response) => {
   res.json({ message: "sogir uddin 220" });
